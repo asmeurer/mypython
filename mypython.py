a4f938dd9d6cb11aaf3e150e3e14d31e1d61d7f4
Aaron Meurer <asmeurer@gmail.com>
2017-01-29 22:09:13 -0500

    Add history

--- mypython.py
+++ mypython.py
@@ -8,6 +8,7 @@ from pygments import highlight
 from prompt_toolkit.shortcuts import prompt
 from prompt_toolkit.layout.lexers import PygmentsLexer
 from prompt_toolkit.styles import style_from_pygments
+from prompt_toolkit.history import InMemoryHistory
 
 from traceback import format_exc
 
@@ -15,10 +16,13 @@ if __name__ == '__main__':
     prompt_number = 1
     _globals = globals().copy()
     _locals = {}
+    history = InMemoryHistory()
     while True:
         try:
             command = prompt('In [%s]: ' % prompt_number,
-                lexer=PygmentsLexer(PythonLexer), style=style_from_pygments(MonokaiStyle))
+                lexer=PygmentsLexer(PythonLexer),
+                style=style_from_pygments(MonokaiStyle), true_color=True,
+                history=history, enable_history_search=False)
         except EOFError:
             break
 
