9e7bc39ee7944d09c0e802286803626a13c4e165
Aaron Meurer <asmeurer@gmail.com>
2017-02-01 00:27:43 -0500

    Add __init__.py

---
+++ mypython/__init__.py
@@ -0,0 +1,21 @@
+from .mypython import (define_custom_keys, PythonSyntaxValidator,
+    get_continuation_tokens, prompt_style, get_prompt_tokens,
+    get_out_prompt_tokens, normalize, startup, main)
+
+__all__ = ['define_custom_keys', 'PythonSyntaxValidator',
+    'get_continuation_tokens', 'prompt_style', 'get_prompt_tokens',
+    'get_out_prompt_tokens', 'normalize', 'startup', 'main']
+
+from .theme import OneAMStyle
+
+__all__ += ['OneAMStyle']
+
+from .multiline import (has_unclosed_brackets, document_is_multiline_python,
+auto_newline)
+
+__all__ += ['has_unclosed_brackets', 'document_is_multiline_python',
+    'auto_newline']
+
+from .completion import get_jedi_script_from_document, PythonCompleter
+
+__all__ += ['get_jedi_script_from_document', 'PythonCompleter']
