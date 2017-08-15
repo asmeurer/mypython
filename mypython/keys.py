071abca45c54a6cb0eca6958390d899dbeb1266e
Aaron Meurer <asmeurer@gmail.com>
2017-08-15 01:27:27 -0400

    Add IPython prompts to the paste regexes

--- mypython/keys.py
+++ mypython/keys.py
@@ -470,8 +470,8 @@ def redo(event):
 
 
 # Need to escape all spaces here because of verbose (x) option below
-ps1_prompts = [r'>>>\ '] + [i*3 + r'\[\d+\]:\ ' for i, j in emoji]
-ps2_prompts = [r'\.\.\.\ ', '\N{CLAPPING HANDS SIGN}+⎢']
+ps1_prompts = [r'>>>\ '] + [i*3 + r'\[\d+\]:\ ' for i, j in emoji] + [r'In\ \[\d+\]:\ ']
+ps2_prompts = [r'\.\.\.\ ', '\N{CLAPPING HANDS SIGN}+⎢'] + [r'\ +\.\.\.: ']
 PS1_PROMPTS_RE = re.compile('|'.join(ps1_prompts))
 PS2_PROMPTS_RE = re.compile('|'.join(ps2_prompts))
 PROMPTED_TEXT_RE = re.compile(r'''(?mx) # Multiline and verbose
