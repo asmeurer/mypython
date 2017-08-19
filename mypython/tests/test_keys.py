6ba4521c6408a732a581025e30d9413c3383495d
Aaron Meurer <asmeurer@gmail.com>
2017-08-19 00:13:15 -0400

    Add tests for split_prompts()

--- mypython/tests/test_keys.py
+++ mypython/tests/test_keys.py
@@ -43,3 +43,92 @@ def test_word_re():
     '345']
     assert WORD.findall("list(WORD.finditer('abc def'))[0].end(0)") == \
         ['list', 'WORD', 'finditer', 'abc', 'def', '0', 'end', '0']
+
+def test_split_prompts():
+    mypython_prompts = """
+    \N{INBOX TRAY}\N{INBOX TRAY}\N{INBOX TRAY}[1]: a = 1
+
+    \N{INBOX TRAY}\N{INBOX TRAY}\N{INBOX TRAY}[2]: a
+    \N{OUTBOX TRAY}\N{OUTBOX TRAY}\N{OUTBOX TRAY}[2]: 1
+
+    \N{INBOX TRAY}\N{INBOX TRAY}\N{INBOX TRAY}[3]: print(a)
+    1
+    \N{OUTBOX TRAY}\N{OUTBOX TRAY}\N{OUTBOX TRAY}[3]: None
+
+    \N{INBOX TRAY}\N{INBOX TRAY}\N{INBOX TRAY}[4]: def test():
+    \N{CLAPPING HANDS SIGN}\N{CLAPPING HANDS SIGN}\N{CLAPPING HANDS SIGN}\N{CLAPPING HANDS SIGN}\N{CLAPPING HANDS SIGN}⎢    for i in range(10):
+    \N{CLAPPING HANDS SIGN}\N{CLAPPING HANDS SIGN}\N{CLAPPING HANDS SIGN}\N{CLAPPING HANDS SIGN}\N{CLAPPING HANDS SIGN}⎢        print(i)
+    """
+
+    python_prompts = """
+>>> a = 1
+>>> a
+1
+>>> print(a)
+1
+>>> def test():
+...     for i in range(10):
+...         print(i)
+    """
+
+    ipython_prompts = """
+In [1]: a = 1
+
+In [2]: a
+Out[2]: 1
+
+In [3]: print(a)
+1
+
+In [4]: def test():
+   ...:     for i in range(10):
+   ...:         print(i)
+   ...:
+    """
+
+    assert split_prompts(mypython_prompts) == split_prompts(python_prompts) == \
+    split_prompts(ipython_prompts) == ['a = 1\n', 'a\n', 'print(a)\n',
+        'def test():\n    for i in range(10):\n        print(i)\n\n']
+
+    assert split_prompts(mypython_prompts, indent='    ') == \
+        split_prompts(python_prompts, indent='    ') == \
+        split_prompts(ipython_prompts, indent='    ') == \
+        ['a = 1\n', '    a\n', '    print(a)\n',
+        '    def test():\n        for i in range(10):\n            print(i)\n\n']
+
+    mypython_magic = """
+\N{SNAKE}\N{SNAKE}\N{SNAKE}[1]: %doctest
+doctest mode enabled
+>>> %sympy
+    import sympy
+    from sympy import *
+    x, y, z, t = symbols('x y z t')
+    k, m, n = symbols('k m n', integer=True)
+    f, g, h = symbols('f g h', cls=Function)
+>>> Integral(x, x)
+Integral(x, x)
+    """
+
+    assert split_prompts(mypython_magic) == ['%doctest\n', '%sympy\n',
+        'Integral(x, x)\n']
+
+    syntax_error = """
+>>> def test():
+...     pass
+>>> a a
+>>> a = 1
+>>> a
+1
+    """
+
+    assert split_prompts(syntax_error) == ['''\
+def test():
+    pass
+
+
+a a
+
+a = 1
+
+a
+''']
