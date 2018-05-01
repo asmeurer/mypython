7f56aeaa21eb0cd4470bcf3f9a64db49682b6dce
Aaron Meurer <asmeurer@gmail.com>
2018-05-01 01:27:18 -0400

    Cleanup imports in __init__.py

--- mypython/__init__.py
+++ mypython/__init__.py
@@ -1,11 +1,10 @@
-from .mypython import (validate_text, PythonSyntaxValidator,
-    get_continuation_tokens, prompt_style, get_in_prompt_tokens,
-    get_out_prompt_tokens, NoResult, smart_eval, normalize, startup,
-    execute_command, run_shell, myhelp, getsource)
+from .mypython import (validate_text, PythonSyntaxValidator, prompt_style,
+    NoResult, smart_eval, normalize, execute_command, run_shell, myhelp,
+    getsource, MyPrompt)
 
-__all__ = ['validate_text', 'PythonSyntaxValidator', 'get_continuation_tokens', 'prompt_style',
-    'get_in_prompt_tokens', 'get_out_prompt_tokens', 'NoResult', 'smart_eval',
-    'normalize', 'startup', 'execute_command', 'run_shell', 'myhelp', 'getsource']
+__all__ = ['validate_text', 'PythonSyntaxValidator', 'prompt_style',
+    'NoResult', 'smart_eval', 'normalize', 'execute_command',
+    'run_shell', 'myhelp', 'getsource', 'MyPrompt']
 
 from .keys import get_key_bindings, custom_key_bindings, split_prompts
 
