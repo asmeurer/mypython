b7ff67be3bd940821594f5f0caa7e36608694720
Aaron Meurer <asmeurer@gmail.com>
2018-08-23 22:48:56 -0600

    Fix split_prompts() for IN/OUT being the full prompt

--- mypython/keys.py
+++ mypython/keys.py
@@ -668,7 +668,7 @@ def redo(event):
 
 
 # Need to escape all spaces here because of verbose (x) option below
-ps1_prompts = [r'>>>\ '] + [re.escape(i)*3 + r'\[\d+\]:\ ' for i, j in emoji] + [r'In\ \[\d+\]:\ ']
+ps1_prompts = [r'>>>\ '] + [re.escape(i) + r'\[\d+\]:\ ' for i, j in emoji] + [r'In\ \[\d+\]:\ ']
 ps2_prompts = [r'\.\.\.\ ', '\N{CLAPPING HANDS SIGN}+⎢'] + [r'\ *\.\.\.:\ ']
 PS1_PROMPTS_RE = re.compile('|'.join(ps1_prompts))
 PS2_PROMPTS_RE = re.compile('|'.join(ps2_prompts))
