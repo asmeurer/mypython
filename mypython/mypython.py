be0483e3c368ac5050c5b3cc13323414ae6ea6a0
Aaron Meurer <asmeurer@gmail.com>
2018-10-12 17:09:46 -0600

    Don't look for <stdin> in getsource
    Mypython no longer sets the filename to <stdin> in doctest mode, so it should
    not be expected any more.

--- mypython/mypython.py
+++ mypython/mypython.py
@@ -331,7 +331,8 @@ def getsource(command, _globals, _locals, ret=False, include_info=True):
             __main__file = sys.modules['__main__'].__file__
         else:
             __main__file = None
-        if filename in ["<stdin>", __main__file] or filename.startswith("<mypython"):
+
+        if filename == [__main__file] or filename.startswith("<mypython"):
             return '\n'.join([i for _, i in sorted(_locals['In'].items())] + ['']).splitlines(keepends=True)
         else:
             return linecache._orig_getlines(filename, module_globals)
