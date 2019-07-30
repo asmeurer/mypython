213841daad3960baf617d4f6ed5b7ddf2c763e3d
Aaron Meurer <asmeurer@gmail.com>
2019-07-30 10:13:11 -0400

    Remove underline from the pyflakes warning style

--- mypython/mypython.py
+++ mypython/mypython.py
@@ -222,8 +222,8 @@ style_extra = {
     Token.MatchingBracket.Other:     "bg:#0000ff", # blue
     Token.MismatchingBracket.Cursor: "italic bg:#ff0000", # red
     Token.MismatchingBracket.Other:  "bg:#ff0000", # red
-    Token.PyflakesWarning.Cursor: "underline fg:#ffafaf", # light pink
-    Token.PyflakesWarning.Other: "underline fg:#ffafaf",
+    Token.PyflakesWarning.Cursor: "fg:#ffafaf", # light pink
+    Token.PyflakesWarning.Other: "fg:#ffafaf",
 }
 
 NO_PROMPT_MODE = False
